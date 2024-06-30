baa42ddedf4106e68f89fda16c4e8e4a17518afd
zabackary <137591653+zabackary@users.noreply.github.com>
2024-06-30 14:47:16 +0900

    chore: move SerialStreamDataSource helpers

--- src/rtd_state/data_source/serial_stream_data_source.rs
+++ src/rtd_state/data_source/serial_stream_data_source.rs
@@ -5,11 +5,49 @@ use tokio_util::codec::{Decoder, Framed};
 
 use crate::codecs::{SerialRTDCodec, SerialRTDCodecError};
 use crate::packet::{Packet, PacketParseError};
+use crate::RTDState;
 
 #[derive(Debug)]
 pub struct SerialStreamDataSource {
     reader: Framed<SerialStream, SerialRTDCodec>,
     ignore_unsupported_packets: bool,
+}
+
+impl RTDState<SerialStreamDataSource> {
+    /// Create a new [`RTDState`] from a serial stream representing a connection
+    /// with the Daktronics All-Sport 5000's serial output. By passing `true` to
+    /// `ignore_unsupported_packets`, it will ignore unsupported packets. As the
+    /// author of the crate doesn't understand the protocol well enough to know
+    /// what every packet means (and thus there are packets that the decoder
+    /// doesn't understand), you should pass `true` as of right now.
+    ///
+    /// The underlying implementation creates an
+    /// [`data_source::SerialStreamDataSource`] which will configure the serial
+    /// stream for you. Thus, don't worry too much about the baud rate and
+    /// parity bit config.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use daktronics_allsport_5000::RTDState;
+    /// # let tty_path = "/dev/ttyUSB0";
+    /// # let baud_rate = 19200;
+    /// # let parity = tokio_serial::Parity::None;
+    /// let serial_stream = tokio_serial::new(tty_path, baud_rate)
+    ///     .parity(parity)
+    ///     .open_native_async()?;
+    /// let rtd_state = RTDState::from_serial_stream(serial_stream, true);
+    /// ```
+    #[cfg(feature = "tokio-serial")]
+    pub fn from_serial_stream(
+        serial_stream: tokio_serial::SerialStream,
+        ignore_unsupported_packets: bool,
+    ) -> Result<Self, tokio_serial::Error> {
+        Ok(Self::new(SerialStreamDataSource::new(
+            serial_stream,
+            ignore_unsupported_packets,
+        )?))
+    }
 }
 
 impl RTDStateDataSource for SerialStreamDataSource {
